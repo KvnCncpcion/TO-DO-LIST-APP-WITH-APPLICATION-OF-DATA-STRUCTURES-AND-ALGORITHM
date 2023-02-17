1a9bba33f156de487d98a0dee7c1eee0ad627bb1
KvnCncpcion <kvncncpcion@gmail.com>
2023-02-17 15:51:25 +0800

    Slight Modifications to the Code V3

--- To-Do-List-App.py
+++ To-Do-List-App.py
@@ -7,7 +7,10 @@ from tkinter import *
 
 root = Tk()
 root.title("To-Do-List")
-root.geometry("595x665+400+100")
+root.geometry("595x667")
+bg = PhotoImage(file = "background.png")
+label = Label(root, image = bg)
+label.place(x=-2,y=-2)
 root.resizable(False,False)
 
 task_list = []
@@ -33,6 +36,9 @@ def deleteTask():
 
         listbox.delete(ANCHOR)
 
+def clearallTask():
+    listbox.delete(0,END)
+
 def openTaskFile():
     try:
         global task_list
@@ -53,16 +59,16 @@ Image_icon = PhotoImage(file="images/task.png")
 root.iconphoto(False,Image_icon)
 
 TopImage = PhotoImage(file="images/topbar.png")
-Label(root,image = TopImage).pack()
+Label(root,image = TopImage, bg="#1A6457").pack()
 
-dockImage = PhotoImage(file = "images/dock2.png")
-Label(root, image = dockImage, bg = "#64B44D").place(x = 75, y = 23)
+dockImage = PhotoImage(file = "images/dock.png")
+Label(root, image = dockImage, bg = "#1CB149").place(x = 75, y = 20)
 
 noteImage = PhotoImage(file = "images/clipboard.png")
-Label(root, image = noteImage, bg = "#64B44D").place(x = 470, y = 23)
+Label(root, image = noteImage, bg = "#8DC53C").place(x = 470, y = 20)
 
 heading = Label(root, text = "ALL TASKS", font = "norwester 33",fg = "black",bg = "#64B44D")
-heading.place(x = 190, y = 13)
+heading.place(x = 190, y = 10)
 
 frame = Frame(root,width=575,height=50,bg="#03093D")
 frame.place(x=10,y=130)
@@ -76,7 +82,7 @@ button = Button(frame,text="ADD",font="norwester 20",width=6,bg="#297061",fg="#f
 button.place(x=490,y=0)
 
 frame1 = Frame(root,bd=3,width=850,height=280,bg="#03093D")
-frame1.pack(pady=(160,0))
+frame1.pack(pady=(120,0))
 
 listbox = Listbox(frame1,font=("comic sans ms",11),width=40,height=16,bg="#64B44D",fg="black",cursor="hand2",selectbackground="#8EFF6D")
 listbox.pack(side=LEFT,fill=BOTH,padx=2)
@@ -89,7 +95,10 @@ scrollbar.config(command=listbox.yview)
 openTaskFile()
 
 Delete_icon = PhotoImage(file="images/delete.png")
-Button(root,image=Delete_icon,bd=0,command=deleteTask).pack(side=BOTTOM,pady=13)
+Button(root,image=Delete_icon,bd=0,command=deleteTask,bg="#33A993").place(x=210,y=575)
+
+clear_icon = PhotoImage(file="images/clear.png")
+Button(root,image=clear_icon,bd=0,command=clearallTask,bg="#33A993").place(x=330,y=575)
 
 root.mainloop()
 
