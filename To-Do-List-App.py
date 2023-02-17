36b6ce44bcacbf42f947022020113533ab088fb9
KvnCncpcion <kvncncpcion@gmail.com>
2023-02-17 11:41:31 +0800

    Slight Modifications to the Code V2

--- To-Do-List-App.py
+++ To-Do-List-App.py
@@ -7,7 +7,7 @@ from tkinter import *
 
 root = Tk()
 root.title("To-Do-List")
-root.geometry("400x665+400+100")
+root.geometry("595x665+400+100")
 root.resizable(False,False)
 
 task_list = []
@@ -55,30 +55,30 @@ root.iconphoto(False,Image_icon)
 TopImage = PhotoImage(file="images/topbar.png")
 Label(root,image = TopImage).pack()
 
-dockImage = PhotoImage(file = "images/dock.png")
-Label(root, image = dockImage, bg = "#32405b").place(x = 30, y = 25)
+dockImage = PhotoImage(file = "images/dock2.png")
+Label(root, image = dockImage, bg = "#64B44D").place(x = 75, y = 23)
 
-noteImage = PhotoImage(file = "images/task.png")
-Label(root, image = noteImage, bg = "#32405b").place(x = 340, y = 25)
+noteImage = PhotoImage(file = "images/clipboard.png")
+Label(root, image = noteImage, bg = "#64B44D").place(x = 470, y = 23)
 
-heading = Label(root, text = "ALL TASKS", font = "norwester 30",fg = "white",bg = "#32405b")
-heading.place(x = 105, y = 20)
+heading = Label(root, text = "ALL TASKS", font = "norwester 33",fg = "black",bg = "#64B44D")
+heading.place(x = 190, y = 13)
 
-frame = Frame(root,width=400,height=50,bg="white")
-frame.place(x=0,y=180)
+frame = Frame(root,width=575,height=50,bg="#03093D")
+frame.place(x=10,y=130)
 
 task = StringVar()
-task_entry = Entry(frame,width=18,font="norwester 20",bd=0)
-task_entry.place(x=10,y=7)
+task_entry = Entry(frame,width=50,font="norwester 23",bd=0,fg="black",bg="#64B44D")
+task_entry.place(x=5,y=5.5)
 task_entry.focus()
 
-button = Button(frame,text="ADD",font="norwester 20",width=6,bg="#5a95ff",fg="#fff",bd=0,command=addTask)
-button.place(x=310,y=0)
+button = Button(frame,text="ADD",font="norwester 20",width=6,bg="#297061",fg="#fff",bd=0,command=addTask)
+button.place(x=490,y=0)
 
-frame1 = Frame(root,bd=3,width=700,height=280,bg="#32405b")
+frame1 = Frame(root,bd=3,width=850,height=280,bg="#03093D")
 frame1.pack(pady=(160,0))
 
-listbox = Listbox(frame1,font=("comic sans ms",11),width=40,height=16,bg="#32405b",fg="white",cursor="hand2",selectbackground="#5a95ff")
+listbox = Listbox(frame1,font=("comic sans ms",11),width=40,height=16,bg="#64B44D",fg="black",cursor="hand2",selectbackground="#8EFF6D")
 listbox.pack(side=LEFT,fill=BOTH,padx=2)
 scrollbar = Scrollbar(frame1)
 scrollbar.pack(side=RIGHT, fill=BOTH)
